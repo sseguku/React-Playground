765fc3fbf7924dd4bbc2db2d306086061500c003
Jjingo Kisakye <sseguku@gmail.com>
2019-10-01 01:42:45 +0300

    Movies-Two tutorial with completed part 1, commencing lists

---
+++ movies-one/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const element = <h1>React - movies on wheels</h1>;
+//console.log(element);
+
+ReactDom.render(element, document.getElementById("root"));
