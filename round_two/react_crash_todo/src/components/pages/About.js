1c8f186ae37d34916d46059d8f2777fbe2a08399
Jjingo Kisakye <sseguku@gmail.com>
2020-04-29 02:52:51 +0300

    re the files and completed tutorial (to_do app) one round two

---
+++ round_two/react_crash_todo/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v.0.1.0. #2020</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
